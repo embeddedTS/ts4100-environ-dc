62a279d5c433523faa18321e99c6e82d448457f3
Kris Bahnsen <kris@embeddedarm.com>
2021-06-29 15:54:14 -0700

    buildroot/board: Add scipt to cycle eth ports
    Save additional power when the ports are unused

--- buildroot/technologic/board/ts4100-environ/post-build.sh
+++ buildroot/technologic/board/ts4100-environ/post-build.sh
@@ -7,6 +7,7 @@ install -m 0644 -D "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/in
 install -m 0644 -D "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/wpa_supplicant.conf "${TARGET_DIR}"/etc/
 
 # Install startup scripts
+install -m 0755 -D "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/S30cycleethports "${TARGET_DIR}"/etc/init.d/
 install -m 0755 -D "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/S35loadwifilcdmodules "${TARGET_DIR}"/etc/init.d/
 install -m 0755 -D "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/S37startenviron "${TARGET_DIR}"/etc/init.d/
 
