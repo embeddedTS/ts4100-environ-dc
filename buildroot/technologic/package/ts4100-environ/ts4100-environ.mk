0ddc7cd486dd4e80d22c89edb43c1e1044ab015e
Kris Bahnsen <kris@embeddedarm.com>
2021-06-29 11:48:04 -0700

    buildroot/package/ts4100_environ: Bump tag to v1.1
    Adds hash file for the release as well

--- buildroot/technologic/package/ts4100-environ/ts4100-environ.mk
+++ buildroot/technologic/package/ts4100-environ/ts4100-environ.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 TS4100_ENVIRON_AUTORECONF = YES
-TS4100_ENVIRON_VERSION = v1.0
+TS4100_ENVIRON_VERSION = v1.1
 TS4100_ENVIRON_SITE = $(call github,embeddedarm,ts4100-environ-dc,$(TS4100_ENVIRON_VERSION))
 
 TS4100_ENVIRON_DEPENDENCIES = host-pkgconf pango cairo fontconfig
