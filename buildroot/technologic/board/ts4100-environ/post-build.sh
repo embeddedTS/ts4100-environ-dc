a3e16d46fb9e1e8485d20b0301f341ba656d7106
Kris Bahnsen <kris@embeddedarm.com>
2021-07-26 13:36:10 -0700

    buildroot/board: Remove dropbear startup in post-build.sh

--- buildroot/technologic/board/ts4100-environ/post-build.sh
+++ buildroot/technologic/board/ts4100-environ/post-build.sh
@@ -19,3 +19,8 @@ install -m 0755 -D "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/S3
 # Create U-Boot script in image, create both .ub and .scr for compat sake
 $MKIMAGE -A arm -T script -C none -n 'readonly' -d "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/tsinit.source "${TARGET_DIR}"/boot/boot.scr
 $MKIMAGE -A arm -T script -C none -n 'readonly' -d "${BR2_EXTERNAL_TS4100_ENVIRON_PATH}"/board/ts4100-environ/tsinit.source "${TARGET_DIR}"/boot/boot.ub
+
+# Remove dropbear server startup if it exists
+if [ -e "${TARGET_DIR}"/etc/init.d/*dropbear ]; then
+	rm "${TARGET_DIR}"/etc/init.d/*dropbear
+fi
